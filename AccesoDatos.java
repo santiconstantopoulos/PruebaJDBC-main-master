654c258c1cb2e6057fd61365a0b6e1e27f5a8b1a
santi <santi@gmail.com>
2022-11-07 23:31:36 -0300

    ddd

--- AccesoDatos.java
+++ AccesoDatos.java
@@ -3,57 +3,54 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class AccesoDatos {
-	 
-	private String  maquina;
-	private String  usuario;
-	private String  clave;
+
+	private String maquina;
+	private String usuario;
+	private String clave;
 	private int puerto;
 	private String bd;
-	private String  servidor;
+	private String servidor;
 	private Connection conexion;
-	
-	public AccesoDatos(String maquina, String usuario, String clave, int puerto, String bd, String servidor, Connection conexion){
-		
-		this.maquina= maquina;
-		this.usuario= usuario;
-		this.clave=clave;
-		this.puerto=puerto;
-		this.bd=bd;
-		this.servidor=servidor;
-		this.conexion= conexion;
-		this.servidor="jdbc:mysql://"+this.maquina+":"+ this.puerto+"/"+this.bd;
-	 
-	        //Carga el driver mysql en memoria
-	    try {
-	            Class.forName("com.mysql.cj.jdbc.Driver");
-	        }
-	    catch (ClassNotFoundException error) {
-	            
-				error.printStackTrace();
-				System.out.println(error.getMessage());
-				System.err.println("ERROR AL REGISTRAR EL DRIVER");
-				
-	            System.exit(0); //parar la ejecuci�n
-	        }
-	 
-	        //Establecer la conexi�n con el servidor y almacenarla en el atributo conexion
-	     try {
-	            conexion = DriverManager.getConnection(this.servidor,
-	                        this.usuario, this.clave);
-	        } catch (SQLException error) { //dfdfdsf
-	            System.err.println("ERROR AL CONECTAR CON EL SERVIDOR");
-	            error.printStackTrace();
-	            System.exit(0); //parar la ejecuci�n
-	        }
-	        //System.out.println("Conectado a ajedrez");
-	    }
-	    //Devuelve el objeto Connection que se usar� en la clase Controlador
-	    public  Connection getConexion() {
-	        return conexion;
-	    }
-	    
-	    
-	    
 
+	public AccesoDatos(String maquina, String usuario, String clave, int puerto, String bd, String servidor,
+			Connection conexion) {
+
+		this.maquina = maquina;
+		this.usuario = usuario;
+		this.clave = clave;
+		this.puerto = puerto;
+		this.bd = bd;
+		this.servidor = servidor;
+		this.conexion = conexion;
+		this.servidor = "jdbc:mysql://" + this.maquina + ":" + this.puerto + "/" + this.bd;
+
+		// Carga el driver mysql en memoria
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException error) {
+
+			error.printStackTrace();
+			System.out.println(error.getMessage());
+			System.err.println("ERROR AL REGISTRAR EL DRIVER");
+
+			System.exit(0); // parar la ejecuci�n
+		}
+
+		// Establecer la conexi�n con el servidor y almacenarla en el atributo conexion
+		try {
+			conexion = DriverManager.getConnection(this.servidor,
+					this.usuario, this.clave);
+		} catch (SQLException error) { // dfdfdsf
+			System.err.println("ERROR AL CONECTAR CON EL SERVIDOR");
+			error.printStackTrace();
+			System.exit(0); // parar la ejecuci�n
+		}
+		// System.out.println("Conectado a ajedrez");
+	}
+
+	// Devuelve el objeto Connection que se usar� en la clase Controlador
+	public Connection getConexion() {
+		return conexion;
+	}
 
 }
